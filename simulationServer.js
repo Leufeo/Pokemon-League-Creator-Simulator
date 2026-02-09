31852267d4c2fff3c48ba1924369e8ca0c54474f
= <=>
2026-02-09 19:08:01 +0100

    added 2 vs 2 simulation backend

--- simulationServer.js
+++ simulationServer.js
@@ -33,9 +33,44 @@ exp.post("/sim/1v1", function (request, result) {
         return
     }
 
+    stream.write(">p1 team 1")
+    stream.write(">p2 team 1")
+
     for (let i = 0;  i < 200;  i++) {
-        moveChoice(stream, 1, properties["left"][Object.keys(properties["left"])[0]])
-        moveChoice(stream, 2, properties["right"][Object.keys(properties["right"])[0]])
+        moveChoiceSingles(stream, 1, properties["left"][Object.keys(properties["left"])[0]])
+        moveChoiceSingles(stream, 2, properties["right"][Object.keys(properties["right"])[0]])
+    }
+})
+
+exp.post("/sim/2v2", function (request, result) {
+    const stream = new Sim.BattleStream()
+    readStream(stream, result);
+
+    stream.write(`>start {"formatid":"[Gen 9] Doubles Custom Game"}`)
+
+    let properties = {}
+    try {
+        properties = addPlayers(stream, ["left", "right"], [pathsTo(request["body"]["left"]), pathsTo(request["body"]["right"])])
+        for (let player in properties) {
+            if (Object.keys(properties[player]).length == 0) {
+                console.log("No sets found for player " + player)
+                throw new Error("No sets found for player " + player)
+            }
+        }
+    } catch {
+        console.log("2v2: Player initialization failed")
+        stream._destroy()
+        return
+    }
+
+    let lead = Math.floor(Math.random() * 2) + 1
+    stream.write(`>p1 team ${lead}${lead % 2 + 1}`)
+    lead = Math.floor(Math.random() * 2) + 1
+    stream.write(`>p2 team ${lead}${lead % 2 + 1}`)
+
+    for (let i = 0;  i < 200;  i++) {
+        playerMoveChoicesDoubles(stream, 1, properties["left"])
+        playerMoveChoicesDoubles(stream, 2, properties["right"])
     }
 })
 
@@ -73,8 +108,8 @@ function addPlayer(stream, number, name, sources) {
     return properties
 }
 
-function moveChoice(stream, playerNumber, setProperties) {
-    const choice = Math.floor(Math.random() * 4) + 1
+function moveChoiceSingles(stream, playerNumber, setProperties) {
+    const choice = Math.floor(Math.random() * setProperties["moves"].length) + 1
     if (setProperties["mega"] && Math.random() < 0.5) {
         stream.write(`>p${playerNumber} move ${choice} mega`)
         setProperties["mega"] = false
@@ -87,6 +122,39 @@ function moveChoice(stream, playerNumber, setProperties) {
         stream.write(`>p${playerNumber} move ${choice}`)
     }
 
+}
+
+function playerMoveChoicesDoubles(stream, playerNumber, sideProperties) {
+    const pokemon1 = stream.battle.sides[playerNumber - 1].active[0]
+    const pokemon2 = stream.battle.sides[playerNumber - 1].active[1]
+    if (stream.battle.sides[playerNumber - 1].pokemonLeft == 2) {
+        stream.write(`>p${playerNumber} ${getMoveChoiceStringDoubles(sideProperties[pokemon1.name], pokemon1)}, ${getMoveChoiceStringDoubles(sideProperties[pokemon2.name], pokemon2)}`)
+    }
+    else if (stream.battle.sides[playerNumber - 1].active[0].hp > 0){
+        stream.write(`>p${playerNumber} ${getMoveChoiceStringDoubles(sideProperties[pokemon1.name], pokemon1)}`)
+    }
+    else if (stream.battle.sides[playerNumber - 1].active[1].hp > 0){
+        stream.write(`>p${playerNumber} ${getMoveChoiceStringDoubles(sideProperties[pokemon2.name], pokemon2)}`)
+    }
+}
+
+function getMoveChoiceStringDoubles(setProperties, pokemonStatus) {
+    const choice = Math.floor(Math.random() * setProperties["moves"].length) + 1
+    let choiceString = "move " + choice
+    if (setProperties["moves"][choice - 1].target == 'normal' && !Object.keys(pokemonStatus.volatiles).includes('mustrecharge')) { // compatibility with special cases like pokemon shapeshifting needs to be tested
+        choiceString += " " + (Math.floor(Math.random() * 2) + 1)
+    }
+
+    if (setProperties["mega"] && Math.random() < 0.5) {
+        choiceString += " mega"
+        setProperties["mega"] = false
+    }
+    else if (setProperties["z"] && Math.random() < 0.5 && setProperties["moves"][choice - 1].type == zCristalType[setProperties["item"]]) {
+        choiceString += " zmove"
+        setProperties["z"] = false
+    }
+
+    return choiceString
 }
 
 function readSets(sources) {
@@ -119,13 +187,20 @@ function mergeSets(sets) {
 
 function getProperties(set) {
     const properties = {}
+
     properties["title"] = removeLastSpaces(set.slice(0, set.indexOf("\n") - 1))
     if (properties["title"].indexOf("@") > -1) {
         properties["item"] = properties["title"].slice(properties["title"].indexOf("@") + 2)
     }
-    properties["moves"] = [getMoveFromSet(set, 1), getMoveFromSet(set, 2), getMoveFromSet(set, 3), getMoveFromSet(set, 4)]
+
+    properties["moves"] = []
+    for (let i = 1; i <= set.match(new RegExp("\n- ", 'g')).length; i++) {
+        properties["moves"].push(getMoveFromSet(set, i))
+    }
+
     properties["mega"] = properties["title"].slice(properties["title"].length - 4).indexOf("te") > -1
     properties["z"] = properties["title"].charAt(properties["title"].length - 1) == "Z"
+
     if (properties["title"].indexOf(" (") > -1) {
         properties["title"] = properties["title"].slice(0, properties["title"].indexOf(" ("))
     }
@@ -156,6 +231,14 @@ function removeLastSpaces(str) {
         i++
     }
     return str.slice(0, str.length - i + 1)
+}
+
+function pathsTo(setNames) {
+    const paths = []
+    for (name of setNames) {
+        paths.push(setDir + name + ".txt")
+    }
+    return paths
 }
 
 const zCristalType = {
